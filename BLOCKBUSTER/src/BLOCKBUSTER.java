8a247282f44dbfd72fbdd813a690c6cf17227fd9
Jack Convis <jackconvis@gmail.com>
2022-05-16 16:49:12 -0400

    Update BLOCKBUSTER.java

--- BLOCKBUSTER/src/BLOCKBUSTER.java
+++ BLOCKBUSTER/src/BLOCKBUSTER.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class BlOCKBUSTER {
+public class BLOCKBUSTER {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner (System.in);
